06aae9051e10b8f32d0bf2aa88c04ba5a506cf6a
bravenewcode <bravenewcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-05-04 05:14:16 +0000

    Revised plugin description
    git-svn-id: https://plugins.svn.wordpress.org/wptouch/trunk@44171 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wptouch.php
+++ wptouch.php
@@ -3,7 +3,7 @@
    Plugin Name: WPtouch Theme & Plugin
    Plugin URI: http://bravenewcode.com/wptouch/
    Description: A plugin which formats your site when viewing with an <a href="http://www.apple.com/iphone/">iPhone</a> / <a href="http://www.apple.com/ipodtouch/">iPod touch</a>. Set header, page, and icon options for the theme by visiting the WPtouch admin panel under Options (wp2.1-3) or Settings (wp2.5) tab. You'll also find a compatibility suite for aspects of your WordPress configuration. &nbsp;
-   Author: Duane Storey & Dale Mugford
+   Author: Dale Mugford & Duane Storey
    Version: 1.0.2
    Author URI: http://www.bravenewcode.com
    
