748966c23b12887e43d742c2c354c51364bd3f50
bravenewcode <bravenewcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-06-11 03:19:05 +0000

    Commented the skipper js out
    git-svn-id: https://plugins.svn.wordpress.org/wptouch/tags/1.0.7@50146 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- themes/default/js/global.js
+++ themes/default/js/global.js
@@ -1,9 +1,10 @@
-addEventListener("load",function()
-{setTimeout(updateLayout,0);setTimeout(function(){window.scrollTo(0,1);},100);},false);var currentWidth=0;function updateLayout()
-{if(window.innerWidth!=currentWidth)
-{currentWidth=window.innerWidth;var orient=currentWidth==320?"profile":"portrait";document.body.setAttribute("orient",orient);setTimeout(function()
-{window.scrollTo(0,1);},500);}}
-setInterval(updateLayout,400);function commentAdded(request){if($('errors')){Element.remove('errors');}
+// addEventListener("load",function()
+// {setTimeout(updateLayout,0);setTimeout(function(){window.scrollTo(0,1);},100);},false);var currentWidth=0;function updateLayout()
+// {if(window.innerWidth!=currentWidth)
+// {currentWidth=window.innerWidth;var orient=currentWidth==320?"profile":"portrait";document.body.setAttribute("orient",orient);setTimeout(function()
+// {window.scrollTo(0,1);},500);}}
+// setInterval(updateLayout,400);
+function commentAdded(request){if($('errors')){Element.remove('errors');}
 new Effect.Appear($('commentlist').lastChild)
 $('comment').value=''
 $('comment').disabled=true
