98f833ea34ef253f767bc6062ca681b095189b8f
bravenewcode <bravenewcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-08-21 06:27:17 +0000

    Optimizations
    git-svn-id: https://plugins.svn.wordpress.org/wptouch/trunk@60828 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- themes/default/header.php
+++ themes/default/header.php
@@ -4,6 +4,9 @@
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 <title><?php bloginfo('name'); ?></title>
 <meta name="generator" content="WordPress <?php bloginfo('version'); ?>" /> <!-- leave this for stats -->
+<meta name="description" content="<?php bloginfo('description'); ?>" />
+<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 <!--
 Strict viewport options to control how the content is shown. 
 Increase the maximum-scale number to allow for zooming if you wish
