99b9286a6f606176b8fe3cdebae09fc0219c5321
bravenewcode <bravenewcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-14 01:23:43 +0000

    Fixed bug
    git-svn-id: https://plugins.svn.wordpress.org/wptouch/trunk@94482 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wptouch.php
+++ wptouch.php
@@ -124,7 +124,7 @@ function detectAppleMobile($query = '') {
 
 // Add whatever user agents you want here to the array if you want to make this show on a Blackberry 
 // or something. No guarantees it'll look pretty, though!
-	$useragents = array("iphone", "ipod", "aspen", "safari", "incognito", "webmate");
+	$useragents = array("iphone", "ipod", "aspen", "incognito", "webmate");
 		$this->applemobile = false;
 			foreach ($useragents as $useragent) {
 			if (eregi($useragent, $container)) {
