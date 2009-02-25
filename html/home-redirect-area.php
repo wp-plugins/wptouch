266d546957dc0f7d052b24f2a4e371aef1bc1f6c
bravenewcode <bravenewcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-25 22:55:47 +0000

    Added language support
    git-svn-id: https://plugins.svn.wordpress.org/wptouch/trunk@97719 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- html/home-redirect-area.php
+++ html/home-redirect-area.php
@@ -1,18 +1,18 @@
 
 <div class="wptouch-itemrow home-page-block">
 	<div class="wptouch-item-desc">
-		<h2>Home Page Redirection</h2>
-		<p>For your home page, WPtouch respects the front page behavior you've defined in the <a href="options-reading.php">WordPress -> Reading Options.</a></p>
+		<h2><?php _e( "Home Page Redirection", "wptouch" ); ?></h2>
+		<p><?php _e( printf( "For your home page, WPtouch respects the front page behavior you've defined in the %sWordPress -> Reading Options%s.', '<a href="options-reading.php">', '</a>' ), "wptouch" ); ?></p>
 	</div>
 
 	<div class="wptouch-item-content-box1">
 		<div class="header-item-desc">
-			If you'd like a different home page (your posts page for example)<br />select it from the list below.<br /><br />
+			<?php _e( "If you'd like a different home page (your posts page for example)", "wptouch" ); ?><br /><?php _e( "select it from the list below.", "wptouch" ); ?><br /><br />
 		</div>
 	
 		<div class="wptouch-select-row">
 			<div class="wptouch-select-left">
-				<label for="home-page">Override Home Page</label>
+				<label for="home-page"><?php _e( "Override Home Page", "wptouch" ); ?></label>
 			</div>
 			
 			<div class="wptouch-select-right">
