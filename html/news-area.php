29367f8a26303ef5fe7071c807c8e1fc9ff8b29b
bravenewcode <bravenewcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-03 05:44:29 +0000

    Added last five donations
    git-svn-id: https://plugins.svn.wordpress.org/wptouch/trunk@99094 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- html/news-area.php
+++ html/news-area.php
@@ -10,13 +10,23 @@
 	<div class="wptouch-item-content-box1">
 		<div id="wptouch-news-frame" style="display: none;"></div>
 
+
 		<script type="text/javascript">
 			jQuery.ajax({
 				url: "<?php bloginfo('wpurl'); ?>/wp-content/plugins/wptouch/load-news.php",
 				success: function(data) {
 					jQuery("#wptouch-news-frame").html(data).fadeIn();
 				}});
+
+			jQuery.ajax({
+				url: "<?php bloginfo('wpurl'); ?>/wp-content/plugins/wptouch/load-news.php?donations=1",
+				success: function(data) {
+					jQuery("#wptouch-donation-frame").html(data).fadeIn();
+				}});
 		</script>
+
+
+		
 	</div>
 	
    <div id="wptouch-news-donate">
@@ -25,6 +35,9 @@
 	  <?php echo sprintf( __( "WPtouch represents hundreds of hours of development work. If you like the project and want to see it continue, %splease consider donating to WPtouch%s.", "wptouch"), '<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&amp;business=paypal%40bravenewcode%2ecom&amp;item_name=WPtouch%20Beer%20Fund&amp;no_shipping=1&amp;tax=0&amp;currency_code=CAD&amp;lc=CA&amp;bn=PP%2dDonationsBF&amp;charset=UTF%2d8">', '</a>' ); ?><br /><br />
 	  
 	  <?php echo sprintf( __( "Everyone who donates will be added to our WPtouch friends and family listing on %sbravenewcode.com/wptouch%s, in appreciation for the support.", "wptouch"), '<a href="http://www.bravenewcode.com/wptouch">', '</a>' ); ?>
+
+	<h3><?php _e( "Last Donations", "wptouch" ); ?></h3>		
+	<ul id="wptouch-donation-frame" style="display: none;"></ul>
    </div>
 	
 	<div class="wptouch-clearer"></div>
