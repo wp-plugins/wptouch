da9631e229f02457abac5b0a5d2253885f992851
bravenewcode <bravenewcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-21 01:31:42 +0000

    Added respect for Allow Comments checkbox on pages
    git-svn-id: https://plugins.svn.wordpress.org/wptouch/trunk@103869 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- html/javascript-area.php
+++ html/javascript-area.php
@@ -17,26 +17,26 @@
 	<div class="wptouch-item-content-box1">
 		<div class="wptouch-checkbox-row">
 			<input type="checkbox" name="enable-cats-button" <?php if ( isset( $wptouch_settings['enable-cats-button']) && $wptouch_settings['enable-cats-button'] == 1) echo('checked'); ?> />
-			<label for="enable-cats-button"> <?php _e( "Enable Categories In The Header", "wptouch" ); ?> (<small><?php _e( "will add a categories button beside search &amp; menu buttons", "wptouch" ); ?>)</small></label>
+			<label for="enable-cats-button"> <?php _e( "Enable categories in the header", "wptouch" ); ?> (<small><?php _e( "will add a categories button beside search &amp; menu buttons", "wptouch" ); ?>)</small></label>
 		</div>
 		<div class="wptouch-checkbox-row">
 			<input type="checkbox" name="enable-login-button" <?php if (isset($wptouch_settings['enable-login-button']) && $wptouch_settings['enable-login-button'] == 1) echo('checked'); ?> />
-			<label for="enable-login-button"> <?php _e( "Enable Login From The Header", "wptouch" ); ?> <small>(<?php _e( "will add a login button beside search &amp; menu buttons", "wptouch" ); ?>)</small></label>
+			<label for="enable-login-button"> <?php _e( "Enable login from the header", "wptouch" ); ?> <small>(<?php _e( "will add a login button beside search &amp; menu buttons", "wptouch" ); ?>)</small></label>
 		</div>
 		
 		<div class="wptouch-checkbox-row">
 			<input type="checkbox" name="enable-js-header" <?php if (isset($wptouch_settings['enable-js-header']) && $wptouch_settings['enable-js-header'] == 1) echo('checked'); ?> />
-			<label for="enable-js-header"> <?php _e( "Use Advanced", "wptouch" ); ?> <a href="http://www.jquery.com/" target="_blank"><?php _e( "jQuery", "wptouch" ); ?></a> <?php _e( "Javascript Effects", "wptouch" ); ?><small>(<?php _e( "ajax entries &amp; comments", "wptouch" ); ?>)</small></label>
+			<label for="enable-js-header"> <?php _e( "Use advanced", "wptouch" ); ?> <a href="http://www.jquery.com/" target="_blank"><?php _e( "jQuery", "wptouch" ); ?></a> <?php _e( "effects", "wptouch" ); ?><small>(<?php _e( "ajax entries &amp; comments", "wptouch" ); ?>)</small></label>
 		</div>
 		
 		<div class="wptouch-checkbox-row">
 			<input type="checkbox" name="enable-gravatars" <?php if (isset($wptouch_settings['enable-gravatars']) && $wptouch_settings['enable-gravatars'] == 1) echo('checked'); ?> />
-			<label for="enable-gravatars"> <?php _e( "Enable Gravatars in Comments", "wptouch" ); ?></label>
+			<label for="enable-gravatars"> <?php _e( "Enable gravatars in comments", "wptouch" ); ?></label>
 		</div>
 		
 		<div class="wptouch-checkbox-row">
 			<input type="checkbox" name="enable-page-coms" <?php if (isset($wptouch_settings['enable-page-coms']) && $wptouch_settings['enable-page-coms'] == 1) echo('checked'); ?> />
-			<label for="enable-page-coms"> <?php _e( "Enable Comments For Pages", "wptouch" ); ?> <small>(<?php _e( "will add the comment form to", "wptouch" ); ?> <strong><?php _e( "all", "wptouch" ); ?></strong> <?php _e( "pages by default", "wptouch" ); ?>)</small></label>
+			<label for="enable-page-coms"> <?php _e( "Enable comments on pages", "wptouch" ); ?> <small>(<?php _e( "will add the comment form to pages with 'Allow Comments' checked in the WordPress admin", "wptouch" ); ?>)</small></label>
 		</div>
 		
 		<div class="wptouch-checkbox-row">
