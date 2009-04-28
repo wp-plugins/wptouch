9d9fca63d1e0497275b62ba0187fcc1697179543
bravenewcode <bravenewcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-28 18:13:39 +0000

    Updated
    git-svn-id: https://plugins.svn.wordpress.org/wptouch/trunk@114069 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- themes/default/header.php
+++ themes/default/header.php
@@ -1,4 +1,4 @@
 <?php 
-require_once('' . compat_get_wp_content_url() . '/plugins/wptouch/themes/core/core-header.php' ); 
+include( dirname(__FILE__) . '/../core/core-header.php' ); 
 // End WPtouch Core Header
 ?>
