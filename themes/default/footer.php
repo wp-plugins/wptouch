3acabf226e062aa3f27aed8d593897b34e7823d7
bravenewcode <bravenewcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-04 12:41:59 +0000

    New Footer appearance
    git-svn-id: https://plugins.svn.wordpress.org/wptouch/trunk@115454 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- themes/default/footer.php
+++ themes/default/footer.php
@@ -1,19 +1,18 @@
 <!-- Here the switch code is very important, as well as the php code which deals with admin links and WordPress -->
 	<div id="footer">
-	<ul>
+	<ul id="links">
 		<?php if (current_user_can('edit_posts')) : ?>
-			<li><a href="<?php bloginfo('wpurl'); ?>/wp-admin/"><?php _e("Admin", "wptouch"); ?></a></li>
+			<li><a class="text" href="<?php bloginfo('wpurl'); ?>/wp-admin/"><?php _e("Admin", "wptouch"); ?></a></li>
 		<?php endif; ?>
-			<li><a href="<?php bloginfo('wpurl'); ?>/wp-admin/profile.php"><?php _e( "Account Profile", "wptouch" ); ?></a></li>
 		<?php if (get_option('comment_registration')) { ?>
-			<li><a href="<?php bloginfo('wpurl'); ?>/wp-register.php"><?php _e( "Register for this site", "wptouch" ); ?></a></li>
+			<li><a class="text" href="<?php bloginfo('wpurl'); ?>/wp-register.php"><?php _e( "Register for this site", "wptouch" ); ?></a></li>
 		<?php } ?>
 		<?php if (is_user_logged_in()) { ?>
-			<li><a href="<?php $version = (float)get_bloginfo('version'); if ($version >= 2.7) { ?><?php echo wp_logout_url($_SERVER['REQUEST_URI']); ?><?php } else { ?><?php bloginfo('wpurl'); ?>/wp-login.php?action=logout&redirect_to=<?php echo $_SERVER['REQUEST_URI']; ?><?php } ?>"><?php _e( "Logout", "wptouch" ); ?></a></li>	
+			<li><a class="text" href="<?php bloginfo('wpurl'); ?>/wp-admin/profile.php"><?php _e( "Account Profile", "wptouch" ); ?></a></li>
+			<li><a class="text" href="<?php $version = (float)get_bloginfo('version'); if ($version >= 2.7) { ?><?php echo wp_logout_url($_SERVER['REQUEST_URI']); ?><?php } else { ?><?php bloginfo('wpurl'); ?>/wp-login.php?action=logout&redirect_to=<?php echo $_SERVER['REQUEST_URI']; ?><?php } ?>"><?php _e( "Logout", "wptouch" ); ?></a></li>	
 		<?php } ?>
+			 <li><a onclick="javascript:document.getElementById('switch-on').style.display='none';javascript:document.getElementById('switch-off').style.display='block';" href="<?php echo bloginfo('home') . '/?bnc_view=normal'; ?>"class="text"><?php _e( "Mobile Theme", "wptouch" ); ?></a><img id="switch-on" src="<?php echo compat_get_plugin_url( 'wptouch' ); ?>/images/on.jpg" alt="on switch image" class="wptouch-switch-image" /><img id="switch-off" style="display:none" src="<?php echo compat_get_plugin_url( 'wptouch' ); ?>/images/off.jpg" alt="off switch image" class="wptouch-switch-image" /></li>
 	</ul>
-
-			<div id="wptouch-switch-link"><?php _e( "Mobile Theme", "wptouch" ); ?><a onclick="javascript:document.getElementById('switch-on').style.display='none';javascript:document.getElementById('switch-off').style.display='block';" href="<?php echo bloginfo('home') . '/?bnc_view=normal'; ?>"><img id="switch-on" src="<?php echo compat_get_plugin_url( 'wptouch' ); ?>/images/on.jpg" alt="on switch image" class="wptouch-switch-image" /><img id="switch-off" style="display:none" src="<?php echo compat_get_plugin_url( 'wptouch' ); ?>/images/off.jpg" alt="off switch image" class="wptouch-switch-image" /></a></div>	
 
 			<?php _e( "All content Copyright &copy;", "wptouch" ); ?> <?php $str = bnc_get_header_title(); echo stripslashes($str); ?>
 			<br />
