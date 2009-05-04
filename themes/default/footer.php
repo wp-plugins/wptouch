8ff6fecffb335487c7817d71eda57abb8d0ac0bc
bravenewcode <bravenewcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-04 12:21:06 +0000

    more core changes
    git-svn-id: https://plugins.svn.wordpress.org/wptouch/trunk@115452 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- themes/default/footer.php
+++ themes/default/footer.php
@@ -1,24 +1,17 @@
 <!-- Here the switch code is very important, as well as the php code which deals with admin links and WordPress -->
 	<div id="footer">
-		<center><h3><?php if (current_user_can('edit_posts')) : // If it's not an admin don't show these! ?>      
-				<a href="<?php bloginfo('wpurl'); ?>/wp-admin/"><?php _e("Admin", "wptouch"); ?></a> | 
-					<?php //Another WordPress version check to figure out the correct logout method
-						$version = (float)get_bloginfo('version'); 
-						if ($version >= 2.7) { ?>
-							<a href="<?php echo wp_logout_url($_SERVER['REQUEST_URI']); ?>">
-						<?php } else { ?>
-							<a href="<?php bloginfo('wpurl'); ?>/wp-login.php?action=logout&redirect_to=<?php echo $_SERVER['REQUEST_URI']; ?>">
-						<?php } ?><?php _e( "Logout", "wptouch" ); ?></a>
-				<?php elseif (current_user_can('read_posts')) : ?>
-					<a href="<?php bloginfo('wpurl'); ?>/wp-admin/profile.php"><?php _e( "Account Profile", "wptouch" ); ?></a><?php if (!bnc_is_login_button_enabled()) { ?> | <a href="<?php echo wp_logout_url(); ?>"><?php _e( "Logout", "wptouch" ); ?></a><?php } ?>
-				<?php else : ?>
-					<?php if (!bnc_is_login_button_enabled() && get_option('comment_registration') && !$user_ID) { ?>
-						<a href="<?php bloginfo('wpurl'); ?>/wp-login.php"><?php _e( "Login to", "wptouch" ); ?> <?php bloginfo('name'); ?></a> | 
-					<?php } ?>
-					<?php if (get_option('comment_registration')) { ?>
-						<a href="<?php bloginfo('wpurl'); ?>/wp-register.php"><?php _e( "Register for this site", "wptouch" ); ?></a>
-					<?php } ?>
-				<?php endif; ?></h3></center>
+	<ul>
+		<?php if (current_user_can('edit_posts')) : ?>
+			<li><a href="<?php bloginfo('wpurl'); ?>/wp-admin/"><?php _e("Admin", "wptouch"); ?></a></li>
+		<?php endif; ?>
+			<li><a href="<?php bloginfo('wpurl'); ?>/wp-admin/profile.php"><?php _e( "Account Profile", "wptouch" ); ?></a></li>
+		<?php if (get_option('comment_registration')) { ?>
+			<li><a href="<?php bloginfo('wpurl'); ?>/wp-register.php"><?php _e( "Register for this site", "wptouch" ); ?></a></li>
+		<?php } ?>
+		<?php if (is_user_logged_in()) { ?>
+			<li><a href="<?php $version = (float)get_bloginfo('version'); if ($version >= 2.7) { ?><?php echo wp_logout_url($_SERVER['REQUEST_URI']); ?><?php } else { ?><?php bloginfo('wpurl'); ?>/wp-login.php?action=logout&redirect_to=<?php echo $_SERVER['REQUEST_URI']; ?><?php } ?>"><?php _e( "Logout", "wptouch" ); ?></a></li>	
+		<?php } ?>
+	</ul>
 
 			<div id="wptouch-switch-link"><?php _e( "Mobile Theme", "wptouch" ); ?><a onclick="javascript:document.getElementById('switch-on').style.display='none';javascript:document.getElementById('switch-off').style.display='block';" href="<?php echo bloginfo('home') . '/?bnc_view=normal'; ?>"><img id="switch-on" src="<?php echo compat_get_plugin_url( 'wptouch' ); ?>/images/on.jpg" alt="on switch image" class="wptouch-switch-image" /><img id="switch-off" style="display:none" src="<?php echo compat_get_plugin_url( 'wptouch' ); ?>/images/off.jpg" alt="off switch image" class="wptouch-switch-image" /></a></div>	
 
