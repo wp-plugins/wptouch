3bff513025b52eb032b8794ad974b74b699c43b5
bravenewcode <bravenewcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-05 20:06:19 +0000

    Updated admin html structure
    git-svn-id: https://plugins.svn.wordpress.org/wptouch/trunk@115790 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- html/home-redirect-area.php
+++ html/home-redirect-area.php
@@ -1,11 +1,10 @@
 <div class="metabox-holder">
 	<div class="postbox">
-
 		<h3 class='hndle'><span><?php _e( "Home Page Redirection", "wptouch" ); ?></span></h3>
 
 			<div class="wptouch-left-content">
-			<p><?php echo sprintf( __( "For your home page, WPtouch respects the front page behavior you've defined in the %sWordPress &raquo; Reading Options%s.", "wptouch"), '<br /><a href="options-reading.php">', '</a>' ); ?></p>
-			<p><?php _e( "If you'd like a different home page for your WPtouch mobile site", "wptouch" ); ?><br /><?php _e( "(your posts page for example) select it from the list below.", "wptouch" ); ?></p>
+				<p><?php echo sprintf( __( "For your home page, WPtouch respects the front page behavior you've defined in the %sWordPress &raquo; Reading Options%s.", "wptouch"), '<br /><a href="options-reading.php">', '</a>' ); ?></p>
+				<p><?php _e( "If you'd like a different home page for your WPtouch mobile site", "wptouch" ); ?><br /><?php _e( "(your posts page for example) select it from the list below.", "wptouch" ); ?></p>
 			</div>
 
 			<div class="wptouch-right-content">
@@ -17,6 +16,6 @@
 				</div>
 			</div>
 
-	</div>
-<div class="wptouch-clearer"></div>
-</div>
+	</div><!-- postbox -->
+	<div class="wptouch-clearer"></div>
+</div><!-- metabox -->
