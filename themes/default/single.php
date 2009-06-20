2ad2f2a5a55427359e95f6254d1fc157bbbce4b4
bravenewcode <bravenewcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-20 22:23:31 +0000

    check if comments are open before showing the comments link
    git-svn-id: https://plugins.svn.wordpress.org/wptouch/trunk@127920 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- themes/default/single.php
+++ themes/default/single.php
@@ -8,16 +8,16 @@
 			        <div class="single-post-meta-top"><?php echo get_the_time('M jS, Y @ h:i a') ?> &rsaquo; <?php the_author() ?><br />
 
 		<!-- Let's check for DISQUS... we need to skip to a different div if it's installed and active -->		
-	
+		<?php if ('open' == $post->comment_status)) : ?>
 		<?php if (function_exists('dsq_comments_template')) { ?>
 	 		<a href="#dsq-add-new-comment">&darr; <?php _e( "Skip to comments", "wptouch" ); ?></a></div>
 		<?php } elseif (function_exists('id_comments_template')) { ?>
 			<a href="#idc-container-parent">&darr; <?php _e( "Skip to comments", "wptouch" ); ?></a></div>
 		<?php } elseif (isset($post->comment_count) && $post->comment_count == 0) { ?>
-			<a href="#com-head">&darr; <?php _e( "Leave a comment", "wptouch" ); ?></a></div>
+			<a href="#respond">&darr; <?php _e( "Leave a comment", "wptouch" ); ?></a></div>
 		<?php } else { ?>
 			<a href="#com-head">&darr; <?php _e( "Skip to comments", "wptouch" ); ?></a></div>		
-		<?php } ?>	
+		<?php } endif; ?>
 		<div class="clearer"></div>
 	</div>
 
