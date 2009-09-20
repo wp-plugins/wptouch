0d5aef1ce833decb035d7c75c86d012fb4e24350
dalemugford <dalemugford@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-20 17:40:57 +0000

    fixed missing </p> closing tag
    git-svn-id: https://plugins.svn.wordpress.org/wptouch/trunk@156525 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- themes/default/comments.php
+++ themes/default/comments.php
@@ -6,7 +6,7 @@
 			// if there's a password
 			if ($_COOKIE['wp-postpass_' . COOKIEHASH] != $post->post_password) {
 			// and it doesn't match the cookie ?>	
-		<p class="nocomments"><?php _e('This post is password protected. Enter the password to view comments.', 'wptouch'); ?><p>
+		<p class="nocomments"><?php _e('This post is password protected. Enter the password to view comments.', 'wptouch'); ?></p>
 <?php return; 	} }
 /* This variable is for alternating comment background */
 $oddcomment = 'alt'; 
