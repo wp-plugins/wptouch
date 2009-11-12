9ed53e9e0dfb3c332935e3066705d95995b6940f
bravenewcode <bravenewcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-11-12 01:52:36 +0000

    twitter refinements
    git-svn-id: https://plugins.svn.wordpress.org/wptouch/trunk@172505 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- themes/core/core-else-menu.php
+++ themes/core/core-else-menu.php
@@ -21,13 +21,12 @@
     	<a id="wordtwitopen" class="top" href="#" onclick="bnc_jquery_wordtwit_open(); return false;"><?php _e( 'Twitter', 'wptouch' ); ?></a>
 	<?php } ?>
 
-
  <!-- #start the Prowl Message Area -->
  <div id="prowl-message" style="display:none">
- 	 <div id="prowl-style-bar"></div><!-- filler to get the styling just right -->
+ 	 <div id="push-style-bar"></div><!-- filler to get the styling just right -->
 	 <img src="<?php echo compat_get_plugin_url( 'wptouch' ); ?>/themes/core/core-images/push-icon.png" alt="push icon" />
-	 <h4><?php _e( 'Send a Push Notification', 'wptouch' ); ?></h4>
-	 <p><?php _e( 'This message will be pushed to the admin\'s iPhone immediately.', 'wptouch' ); ?></p>
+	 <h4><?php _e( 'Send a Message', 'wptouch' ); ?></h4>
+	 <p><?php _e( 'This message will be pushed to the admin\'s iPhone instantly.', 'wptouch' ); ?></p>
 	 
 	 <form id="prowl-direct-message" method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
 	 	<p>
@@ -41,8 +40,8 @@
 		</p>
 
 		<textarea name="prowl-msg-message"></textarea>
-		<input type="hidden" name="wptouch-prowl-message" value="1" /> 	
-		<input type="hidden" name="_nonce" value="<?php echo wp_create_nonce( 'wptouch-prowl' ); ?>" />
+		<input type="hidden" name="wptouch-prowl-message" value="1" /> 
+		<input type="hidden" name="_nonce" value="<?php echo wp_create_nonce( 'wptouch-prowl' ); ?>" />			
 		<input type="submit" name="prowl-submit" value="<?php _e('Send Now', 'wptouch' ); ?>" id="prowl-submit" />
 	 </form>
 	<div class="clearer"></div>
@@ -83,7 +82,7 @@
  <!-- #start the WordTwit Twitter Integration -->
  	<?php $tweets = wordtwit_get_recent_tweets(); ?>
 	<div id="wptouch-wordtwit" class="dropper" style="display:none">
- 	 <div class="wptouch-style-bar"></div><!-- filler to get the styling just right -->
+ 	 <div id="twitter-style-bar"></div><!-- filler to get the styling just right -->
 			<a  id="follow-arrow" href="http://twitter.com/<?php echo wordtwit_get_username(); ?>" target="_blank"><img src="<?php echo compat_get_plugin_url( 'wptouch' ); ?>/themes/core/core-images/twitter-arrow.jpg" alt="follow me" /></a>
 		<div id="wordtwit-avatar">
 			<img src="<?php echo wordtwit_get_profile_url(); ?>" alt="Twitter Avatar" />
