d98c8e9ae51c5cd02b4bd0d527db83abe3e01435
bravenewcode <bravenewcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-18 01:03:56 +0000

    revised php formatting
    git-svn-id: https://plugins.svn.wordpress.org/wptouch/trunk@218945 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ajax/support.php
+++ ajax/support.php
@@ -1,24 +1,34 @@
-<?php require_once( ABSPATH . WPINC . '/feed.php' );
-echo '<ul>';
-if (function_exists('fetch_feed')) {
-// Get a SimplePie feed object from the specified feed source.
-$rss = fetch_feed('http://www.bravenewcode.com/support/rss/forum/wptouch');
-if (!is_wp_error( $rss ) ) : // Checks that the object is created correctly 
-    // Figure out how many total items there are, but limit it to 5. 
-    $maxitems = $rss->get_item_quantity(6);
-    $rss_items = $rss->get_items(0, $maxitems); endif; ?>
+<ul>
+<?php 
+	if ( function_exists( 'fetch_feed' ) ) {
+		require_once(  ABSPATH . WPINC . '/feed.php'  );
+	
+		// Get a SimplePie feed object from the specified feed source.
+		$rss = fetch_feed( 'http://www.bravenewcode.com/support/rss/forum/wptouch' );
 
-    <?php if ($maxitems == 0) echo '<li>No feed items found to display.</li>';
-    else
-    // Loop through each feed item and display each item as a hyperlink.
-    foreach ( $rss_items as $item ) : ?>
-    <li>
-		<a target="_blank" class="orange-link" href='<?php echo $item->get_permalink(); ?>' title='<?php echo 'Posted '.$item->get_date('j F Y | g:i a'); ?>'>
-			<?php echo $item->get_title(); ?>
-		</a>
-    </li>
-    <?php endforeach; ?>
-    <?php } else { ?>
-    <li>No feed items found to display.</li>
-    <?php } ?>
+		// Checks that the object is created correctly 
+		if ( !is_wp_error( $rss ) ) { 
+
+			// Figure out how many total items there are, but limit it to 5. 
+			$maxitems = $rss->get_item_quantity( 6 );
+			$rss_items = $rss->get_items( 0, $maxitems ); 
+		}
+
+		if ( $maxitems == 0 ) {
+			echo '<li>No feed items found to display.</li>';
+		
+		} else {
+			// Loop through each feed item and display each item as a hyperlink.
+			foreach (  $rss_items as $item  ) : ?>
+				<li>
+					<a target="_blank" class="orange-link" href='<?php echo $item->get_permalink(  ); ?>' title='<?php echo 'Posted '.$item->get_date( 'j F Y | g:i a' ); ?>'>
+						<?php echo $item->get_title(  ); ?>
+					</a>
+				</li>
+			<?php endforeach; 
+		} ?>
+		
+	<?php } else {  // if fetch_feed() doesn't exist ?>
+		<li>No feed items found to display.</li>
+	<?php } ?>
 </ul>
