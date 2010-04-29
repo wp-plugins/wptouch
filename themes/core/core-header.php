27387192929b7b637d3c2e8ea36ae6b9b355b656
bravenewcode <bravenewcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-29 20:45:04 +0000

    spelling error fix..
    git-svn-id: https://plugins.svn.wordpress.org/wptouch/trunk@235585 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- themes/core/core-header.php
+++ themes/core/core-header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" manifest="<?php echo compat_get_plugin_url( 'wptouch' ); ?>/themes/core/wptouch.menifest">
+<html xmlns="http://www.w3.org/1999/xhtml" manifest="<?php echo compat_get_plugin_url( 'wptouch' ); ?>/themes/core/wptouch.manifest">
 <head profile="http://gmpg.org/xfn/11">
 	<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" />
 	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
