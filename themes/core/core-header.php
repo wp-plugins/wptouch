af0c18b15896982e9ce2752d43f06fd2b742f917
bravenewcode <bravenewcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-05 01:58:56 +0000

    tagging 1.9.11 for release
    git-svn-id: https://plugins.svn.wordpress.org/wptouch/trunk@237591 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- themes/core/core-header.php
+++ themes/core/core-header.php
@@ -4,7 +4,7 @@
 	<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" />
 	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=no;" />
-<!-- 
+<!-- Web-app mode is not fully supported, yet : )
 	<meta name="apple-mobile-web-app-status-bar-style" content="black">
 	<meta name="apple-mobile-web-app-capable" content="yes">
 -->	
