eebc7d0c61476e0c7a4acc3e572dfeb8ced13bdd
bravenewcode <bravenewcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-27 23:29:14 +0000

    Changed link for WPtouch Pro
    git-svn-id: https://plugins.svn.wordpress.org/wptouch/trunk@416159 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- html/head-area.php
+++ html/head-area.php
@@ -10,7 +10,7 @@
 			</div>
 				<div id="wptouch-head-links">
 					<ul>
-						<li><?php echo sprintf(__( "%sGet WPtouch Pro%s", "wptouch" ), '<a href="http://www.bravenewcode.com/products/wptouch-pro/?utm_source=wptouch&amp;utm_medium=web&amp;utm_campaign=top-' . str_replace( '.', '', $bnc_wptouch_version ) . '" target="_blank">','</a>'); ?> | </li>
+						<li><?php echo sprintf(__( "%sGet WPtouch Pro%s", "wptouch" ), '<a href="http://www.bravenewcode.com/store/plugins/wptouch-pro/?utm_source=wptouch&amp;utm_medium=web&amp;utm_campaign=top-' . str_replace( '.', '', $bnc_wptouch_version ) . '" target="_blank">','</a>'); ?> | </li>
 						<li><?php echo sprintf(__( "%sJoin our FREE Affiliate Program%s", "wptouch" ), '<a href="http://www.bravenewcode.com/affiliate-program/" target="_blank">','</a>'); ?></li> |
 						<li><?php echo sprintf(__( "%sFollow Us on Twitter%s", "wordtwit" ), '<a href="http://www.twitter.com/bravenewcode" target="_blank">','</a>'); ?></li>
 					</ul>
