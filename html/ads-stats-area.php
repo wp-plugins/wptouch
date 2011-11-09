61c6fa5c7c70d3b53de35082ae3815c15088fa4b
bravenewcode <bravenewcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-09 20:09:32 +0000

    Layout changes for advertising
    git-svn-id: https://plugins.svn.wordpress.org/wptouch/trunk@461129 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- html/ads-stats-area.php
+++ html/ads-stats-area.php
@@ -3,26 +3,12 @@
 <div class="metabox-holder">
 	<div class="postbox">
 		<h3><span class="adsense-options">&nbsp;</span><?php _e( "Advertising, Stats &amp; Custom Code", "wptouch" ); ?></h3>
-
+		<div id="advertising-service">
 			<div class="left-content">
 				<h4><?php _e( "Advertising Service", "wptouch" ); ?></h4>
-					<p><?php _e( "Choose which advertising service you would like to use within WPtouch", "wptouch" ); ?></p>
-				<br />
-				<h4><?php _e( "Adsense", "wptouch" ); ?></h4>
-					<p><?php _e( "Enter your Google AdSense ID if you'd like to support mobile advertising in WPtouch posts.", "wptouch" ); ?></p>
-					<p><?php _e( "Make sure to include the 'pub-' part of your ID string.", "wptouch" ); ?></p>
-				<br />
-			    <h4><?php _e( "Stats &amp; Custom Code", "wptouch" ); ?></h4>
-			 		<p><?php _e( "If you'd like to capture traffic statistics ", "wptouch" ); ?><br /><?php _e( "(Google Analytics, MINT, etc.)", "wptouch" ); ?></p>
-			 		<p><?php _e( "Enter the code snippet(s) for your statistics tracking.", "wptouch" ); ?> <?php _e( "You can also enter custom CSS &amp; other HTML code.", "wptouch" ); ?> <a href="#css-info" class="fancylink">?</a></p>
-			 		<div id="css-info" style="display:none">
-					<h2><?php _e( "More Info", "wptouch" ); ?></h2>
-					<p><?php _e( "You may enter a custom css file link easily. Simply add the full link to the css file like this:", "wptouch" ); ?></p>
-					<p><?php _e( "<code>&lt;style type=&quot;text/css&quot;&gt;#mydiv { color: red; }&lt;/style&gt;</code>", "wptouch" ); ?></p>
-				</div>
-
-			</div><!-- left content -->
-
+				<p><?php _e( "Choose which advertising service you would like to use within WPtouch", "wptouch" ); ?></p>
+			</div>
+			
 			<div class="right-content">
 				<ul class="wptouch-make-li-italic">
 					<li>
@@ -30,19 +16,48 @@
 							<option value="none"<?php if ( $wptouch_settings['ad_service'] == 'none') echo " selected"; ?>><?php _e( "None", "wptouch" ); ?></option>
 							<option value="appstores"<?php if ( $wptouch_settings['ad_service'] == 'appstores') echo " selected"; ?>><?php _e( "AppStores", "wptouch" ); ?></option>								
 							<option value="adsense"<?php if ( $wptouch_settings['ad_service'] == 'adsense') echo " selected"; ?>><?php _e( "Google Adsense", "wptouch" ); ?></option>
-			
+		
 						</select>
 						<?php _e( "Advertising Service", "wptouch" ); ?>
 					</li>
-				</ul>
+				</ul>	
+			</div>			
+			<div class="bnc-clearer"></div>
+		</div>
+		
+		<div id="google-adsense">
+			<div class="left-content">
+				<h4><?php _e( "Adsense", "wptouch" ); ?></h4>
+				<p><?php _e( "Enter your Google AdSense ID if you'd like to support mobile advertising in WPtouch posts.", "wptouch" ); ?></p>
+				<p><?php _e( "Make sure to include the 'pub-' part of your ID string.", "wptouch" ); ?></p>
+			</div>
+			
+			<div class="right-content">
 				<ul class="wptouch-make-li-italic">
 					<li><input name="adsense-id" type="text" value="<?php echo $wptouch_settings['adsense-id']; ?>" /><?php _e( "Google AdSense ID", "wptouch" ); ?></li>
 					<li><input name="adsense-channel" type="text" value="<?php echo $wptouch_settings['adsense-channel']; ?>" /><?php _e( "Google AdSense Channel", "wptouch" ); ?></li>
 				</ul>
+			</div>			
+			<div class="bnc-clearer"></div>
+		</div>	
+		
+		<div id="main-stats-area">
+			<div class="left-content">
+		    	<h4><?php _e( "Stats &amp; Custom Code", "wptouch" ); ?></h4>
+		 		<p><?php _e( "If you'd like to capture traffic statistics ", "wptouch" ); ?><br /><?php _e( "(Google Analytics, MINT, etc.)", "wptouch" ); ?></p>
+		 		<p><?php _e( "Enter the code snippet(s) for your statistics tracking.", "wptouch" ); ?> <?php _e( "You can also enter custom CSS &amp; other HTML code.", "wptouch" ); ?> <a href="#css-info" class="fancylink">?</a></p>
+		 		<div id="css-info" style="display:none">
+					<h2><?php _e( "More Info", "wptouch" ); ?></h2>
+					<p><?php _e( "You may enter a custom css file link easily. Simply add the full link to the css file like this:", "wptouch" ); ?></p>
+					<p><?php _e( "<code>&lt;style type=&quot;text/css&quot;&gt;#mydiv { color: red; }&lt;/style&gt;</code>", "wptouch" ); ?></p>			
+				</div>	
+			</div>
 			
+			<div class="right-content">
 				<textarea id="wptouch-stats" name="statistics"><?php echo stripslashes($wptouch_settings['statistics']); ?></textarea>
-
-						</div><!-- right content -->
-		<div class="bnc-clearer"></div>
+			</div>
+			
+			<div class="bnc-clearer"></div>
+		</div>		
 	</div><!-- postbox -->
 </div><!-- metabox -->
