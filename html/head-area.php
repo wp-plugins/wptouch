217162835a469460bd6523a3a811aec4a514f20c
bravenewcode <bravenewcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-08 15:38:40 +0000

    Fixes
    git-svn-id: https://plugins.svn.wordpress.org/wptouch/trunk@753605 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- html/head-area.php
+++ html/head-area.php
@@ -10,9 +10,9 @@
 			</div>
 				<div id="wptouch-head-links">
 					<ul>
-						<li><?php echo sprintf(__( "%sGet WPtouch Pro%s", "wptouch" ), '<a href="http://www.bravenewcode.com/wptouch/?utm_source=wptouch-free&amp;utm_medium=web&amp;utm_campaign=top-' . str_replace( '.', '', $bnc_wptouch_version ) . '" target="_blank">','</a>'); ?> | </li>
-						<li><?php echo sprintf(__( "%sFollow Us on Twitter%s", "wptouch" ), '<a href="http://www.twitter.com/bravenewcode" target="_blank">','</a>'); ?></li> |
-						<li><?php echo sprintf(__( "%sFind Us on Facebook%s", "wptouch" ), '<a href="http://www.facebook.com/bravenewcode" target="_blank">','</a>'); ?></li>
+						<li><?php echo sprintf(__( "%sUpgrade to WPtouch Pro%s", "wptouch" ), '<a href="http://www.bravenewcode.com/wptouch/?utm_source=wptouch-free&amp;utm_medium=web&amp;utm_campaign=top-' . str_replace( '.', '', $bnc_wptouch_version ) . '" target="_blank">','</a>'); ?> | </li>
+						<li><?php echo sprintf(__( "%sWPtouch on Twitter%s", "wptouch" ), '<a href="http://www.twitter.com/bravenewcode" target="_blank">','</a>'); ?> | </li>
+						<li><?php echo sprintf(__( "%sWPtouch on Facebook%s", "wptouch" ), '<a href="http://www.facebook.com/bravenewcode" target="_blank">','</a>'); ?></li>
 					</ul>
 				</div>
 	<div class="bnc-clearer"></div>
