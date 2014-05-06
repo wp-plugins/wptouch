534d4b7deec2e29c73e138396ad2384bc20fc214
bravenewcode <bravenewcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-06 18:36:37 +0000

    Updates
    git-svn-id: https://plugins.svn.wordpress.org/wptouch/trunk@909161 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include/html/footer.php
+++ include/html/footer.php
@@ -5,9 +5,8 @@
 	?>
 	
 	<?php if ( $footer_settings->add_referral_code && $bnc_settings->referral_user_id ) { ?>
-		<?php echo sprintf( __( 'Powered by %s%s%s', 'wptouch-pro' ) , '<a href="http://www.wptouch.com/?wptouch_affiliate_id=' . $bnc_settings->referral_user_id . '" target="_blank">', 'WPtouch Pro ' . WPTOUCH_VERSION, '</a>'); ?>
+		<?php echo sprintf( __( 'Powered By<br/>%s%s%s', 'wptouch-pro' ) , '<a href="http://www.wptouch.com/?wptouch_affiliate_id=' . $bnc_settings->referral_user_id . '" target="_blank">', 'WPtouch Mobile Suite for WordPress', '</a>'); ?>
 	<?php } else { ?>
-		<?php echo sprintf( __( 'Powered by %s%s%s', 'wptouch-pro' ) , '<a href="http://www.wptouch.com/?utm_campaign=wptouch-powered-by&utm_medium=web" target="_blank">', 'WPtouch Pro ' . WPTOUCH_VERSION, '</a>'); ?>
+		<?php echo sprintf( __( 'Powered By<br/>%s%s%s', 'wptouch-pro' ) , '<a href="http://www.wptouch.com/?utm_campaign=wptouch-powered-by&utm_medium=web" target="_blank">', 'WPtouch Mobile Suite for WordPress', '</a>'); ?>
 	<?php } ?><br />
-	<?php echo sprintf( __( 'By %s', 'wptouch-pro' ), 'BraveNewCode Inc.' ); ?>
 </p>
