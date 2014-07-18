38463962ecc9820a7da842b0d1c1bac833eb5eb2
bravenewcode <bravenewcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-18 20:54:08 +0000

    removing 3.4.3
    git-svn-id: https://plugins.svn.wordpress.org/wptouch/trunk@951230 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/settings/html/plugin-compat.php
+++
@@ -1,9 +0,0 @@
-<label class="plugins"><?php _e( "Choose which plugins to include in your mobile theme", "wptouch-pro" ); ?></label>
-<?php include( WPTOUCH_DIR . '/include/html/pro.php' ); ?>
-
-<div id="plugin-compat-setting" class="nano">
-
-	<div class="content">
-		<p><?php _e( 'Your active plugin list is refreshing', 'wptouch-pro' ); ?>&hellip;</p>
-	</div>
-</div>
