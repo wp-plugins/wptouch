fa4cffad2eada464450be22fe2dfc8503adce445
bravenewcode <bravenewcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-26 17:17:32 +0000

    Adding 3.4.6
    git-svn-id: https://plugins.svn.wordpress.org/wptouch/trunk@973445 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ themes/foundation/modules/hammer/wptouch.hammer.js
@@ -0,0 +1,17 @@
+/* WPtouch Foundation Hammer Code */
+
+// WPtouch-specific hammer code should go here
+/* Example:
+
+function doHammerTime() {
+
+
+
+var hammer_options = {};
+jQuery( '#element' ).hammer( hammer_options ).on( 'swipeleft', function( ev ) { console.log( ev ); });
+
+}
+
+jQuery( document ).ready( function() { doHammerTime(); } );
+
+*/
