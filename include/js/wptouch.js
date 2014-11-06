e258d6ba93cee1bf301b38b33c668a60e9fa4f4e
bravenewcode <bravenewcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-06 19:56:35 +0000

    Adding 3.6 codebase
    git-svn-id: https://plugins.svn.wordpress.org/wptouch/trunk@1021138 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ include/js/wptouch.js
@@ -0,0 +1,19 @@
+// WPtouch Basic Client-side Ajax Routines
+function WPtouchAjax( actionName, actionParams, callback ) {
+	var ajaxData = {
+		action: "wptouch_client_ajax",
+		wptouch_action: actionName,
+		wptouch_nonce: WPtouch.security_nonce
+	};
+
+	for ( name in actionParams ) { ajaxData[name] = actionParams[name]; }
+
+	jQuery.post( WPtouch.ajaxurl, ajaxData, function( result ) {
+		callback( result );
+	});
+}
+
+jQuery( '#footer .back-to-top' ).click( function( e ) {
+	e.preventDefault();
+	jQuery( window ).scrollTop( 0 );
+});
---
+++ themes/bauhaus/readme.txt
@@ -0,0 +1,9 @@
+Theme Name: Bauhaus
+Theme URI: http://www.bravenewcode.com/wptouch/
+Description: Clean, modern, functional design. Great for all types of WordPress sites.
+Version: 1.4.3
+Stable tag: 1.4.3
+Depends on: 3.6
+Author: BraveNewCode Inc.
+Parent: Foundation
+Tags: smartphone
