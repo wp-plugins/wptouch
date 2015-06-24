5e189d23ae8d07debfdfdaaaa09477ec9081d523
bravenewcode <bravenewcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-24 20:14:44 +0000

    3.8.1 release files
    git-svn-id: https://plugins.svn.wordpress.org/wptouch/trunk@1187123 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ core/rtl.php
@@ -0,0 +1,18 @@
+<?php
+
+function wptouch_should_load_rtl() {
+	global $wptouch_pro;
+
+	if ( is_admin() ) {
+		$settings = wptouch_get_settings();
+		if ( defined( 'WPTOUCH_ADMIN_IGNORE_LOCALIZATION' ) ) {
+			return false;
+		}
+		/* TODO: Remove deprecated setting
+		if ( !$settings->translate_admin ) {
+			return false;
+		}*/
+	}
+
+	return is_rtl() || $wptouch_pro->locale == 'ar' || $wptouch_pro->locale == 'he_IL';
+}
---
+++ core/theme-hashes.php
@@ -0,0 +1,129 @@
+<?php
+	function wptouch_get_hashes() {
+	$theme_hases = array();
+
+	$theme_hashes[ 'bauhaus' ] = array(
+		'1.0.1' => '4a8191ca6d678e7316c40100f5da6e62',
+		'1.0.2' => '596c46a88f2b36c5b08affc01be0772c',
+		'1.0.3' => '8bf9e89ee26f8c056f34c94da221ee09',
+		'1.0.4' => '186109a5bdf164e2e7abf3d474f844af',
+		'1.0.5' => 'd78c8f124cdff22cae69ab5ee527a92c',
+		'1.0.6' => 'f0b7d32e320fac090e509bfa7fdeee55',
+		'1.0.7' => '12b86776f9c168b56a24ea58f9013eec',
+		'1.0.8' => '0c9e769c2bfb1ec84f367f492485a8ec',
+		'1.0.9' => 'eecd4064953406d9587e9b8889cf15cf',
+		'1.0' => 'a8530fb747ed2321723a92b1af685c07',
+		'1.1' => '43aaa4c67cfb5fd332765dba1e90b8f2',
+		'1.2.1' => '5df2e52222e927b648ccacbb4e426970',
+		'1.2' => 'cdeada535c1f83e4e76a836544eff263',
+		'1.3.1' => '4138eeed1de619272d84b42f433263ff',
+		'1.3' => '50f2677d3dc28e002fb67f4a89af7d29',
+		'1.4.1' => 'e7678f85552de546dca1f1f0f2edac56',
+		'1.4.2' => '1ef9dcde45f9ae4b155ff486f13ebd94',
+		'1.4.3' => '5cd25c32e5c898be3ed74f82cedcf999',
+		'1.4.4' => '346a9811fc01a388aa9d81016342b3ab',
+		'1.4.5' => 'bb7acd8cd44b4689a56d0cc06f6850fb',
+		'1.4.6' => 'dc6306a00fa753701e738b14297c12dd',
+		'1.4.7' => '8f2062860f881205101cc5de916cb1ee',
+		'1.4.8' => '534c5500224b75e045036cb25faae646',
+		'1.4' => 'c59235059ca22f8d41bca4d79a650df0',
+	);
+
+	$theme_hashes[ 'classic-redux' ] = array(
+		'1.1.1' => '407c585ff6ce007cb243fa2336fe33c5',
+		'1.1.2' => 'acab7145a3bf472e8695cbe396f9ff12',
+		'1.1.3' => '104e5b708a815feb4eb15399d77c8e1b',
+		'1.1.4' => '598c598c581c8651ab91408ff1447971',
+		'1.1.5' => '2d6c57d02a1a946bcf4b2b6ade80346b',
+		'1.1.6' => '9c7dc6b1e21b2ccbf4b29b182e75a27f',
+		'1.1' => '1bd2e0cfa3c54690bb07246d103e4fb1',
+		'1.2.1' => '1b6d1d1f1f7d1d0a87b7a116e9aac990',
+		'1.2' => '9acaa0a802ece99a229abdacb50d400a',
+		'1.3' => 'c04f2a4e24f6c1187556e2e74c057369',
+		'1.4.1' => '58c8a27687c37cdc31ee7a54a50ae8e7',
+		'1.4.2' => '51cc6fce56fbfe2dc39a4fa3b5c0b482',
+		'1.4' => '1e47a4aa31dadb8ede23c09ae3310d7c',
+	);
+
+	$theme_hashes[ 'cms' ] = array(
+		'1.1.1' => '308ac26e728c86510e1a673a8c94460f',
+		'1.1.2' => 'c3a9e919007213d3ec5bef6e05b189fe',
+		'1.1.3' => '5be7265ba7f7987a537f52489ee2fd1d',
+		'1.1.4' => '6fea641bbfec3f9982601ff31a88d4d7',
+		'1.1.7' => 'd12742376dde0857dd404fbef25133d4',
+		'1.1' => 'eaf9ae919b89844af381437aba45151b',
+		'1.2.1' => 'ad8befc86d3275845d7385317422095e',
+		'1.2' => '5d4f3ba59119e3d9929b7f2adfa1ba36',
+		'1.3.1' => 'c8a51c3961b0375943f43b6d9bd3d91c',
+		'1.3.2' => '92e27299a3e5a747f1c4a07b14c0b900',
+		'1.3.3' => '114622b6c6affcb084ac0953331d4271',
+		'1.3.5' => '7b8edc0e78c10bb233f91efb88019e6c',
+		'1.3' => '9c0a8c5160771e1879fdc63e88b68705',
+	);
+
+	$theme_hashes[ 'mobilestore' ] = array(
+		'1.0.1' => '5f3ebc75ccfaac0c9dda89c037880c1a',
+		'1.0.10' => 'c6a5f3fdd3d9c0e2459b758fcf3037ed',
+		'1.0.11.1' => 'b2a77e0bbcbbbf08a5a6b45609e2c25b',
+		'1.0.11' => 'b7a96487e60fbc7b79ccd305f507ad24',
+		'1.0.12' => '8ad04cbd909f533d3218c3b0a1b7e49f',
+		'1.0.2' => 'a269b3e46716b4df879fd0cb04a0d1e5',
+		'1.0.3' => 'f1ad6bcc1448680a5cb09a2b4f5bbb12',
+		'1.0.4' => 'ec2bfa70229a0c52c226678893408d23',
+		'1.0.5' => '307775af6f2462dd93a3bdfc30ee0d3d',
+		'1.0.6' => '76805c9bd5fff72f0fe6676cba2a4b89',
+		'1.0.7' => '8a306d19b9fa6fafffaed4169b721039',
+		'1.0.8' => '2b3534dcfdb02321ac5fe66941b1493c',
+		'1.0.9' => '4c9da9be8cbe94b79379116b8d5018f3',
+		'1.0' => 'f0a23183f613a2b2fb0fd3167effad3d',
+		'1.1.1' => '0feff71f14f3deabba64a8d8f12c16f4',
+		'1.1.2' => 'f8e31aa5e39f65bc97c98d3591e8e831',
+		'1.1.3' => '0d46de921e792add4277260d3ad359bc',
+		'1.1' => '6e80a32d2abd10f49a7e37761921c1bd',
+	);
+
+	$theme_hashes[ 'open' ] = array(
+		'1.0' => '6efccd01e67e790922aacec59f2b50e3',
+		'1.1.1' => '71d57664dc4ee2ef375e03844dc1312f',
+		'1.1.3' => '1f8f1f11ea108fac9bf58a5b4a38fec7',
+		'1.1.4' => 'b7865db2d25a298df565e327e77e100c',
+		'1.1' => 'a544b457f58487f11970bfd5ae60288e',
+		'1.2.1' => '7dc79d8895d7f03a2b3217de7a4dfb43',
+		'1.2' => '9ebb04be96635b2ef8fec4a513c8b3ba',
+		'1.3.1' => 'b0fc856d7066920eafb8327e570bc0f1',
+		'1.3.2' => 'a6ce39a72e086925cac0a624963d6dde',
+		'1.3.3' => '86f8115c374bfaa92e83ea2185ab0c44',
+		'1.3' => 'c2a7ae60bde5e1e5788f03b898897712',
+		'1.4.1' => 'dbd2694dfbb6716e4aac66e06f4815e3',
+		'1.4' => '42c7e14aa644079a6157bb4ae770aedd',
+	);
+
+	$theme_hashes[ 'prose' ] = array(
+		'1.0' => '8c612e0876e4ad1235af95d2fc8eeae3',
+		'1.1.1' => 'ebf97bd71ba1693e78b9413db6634826',
+		'1.2' => '7eadcafc919f91b85ad62531d9e8c1da',
+		'1.3' => '9151f9fb5cae05759fa8d679e48363d6',
+		'1.4.1' => '052b18b6b1642e8f89c94fdc214310f5',
+		'1.4.2' => 'b03daf3edd9cd8fd1a9339e861b5606b',
+		'1.4.3' => 'e8135fdc3f26560560add4caaed16384',
+		'1.4.4' => 'd462526da2c757775dd784914b45f141',
+		'1.4.5' => 'abc5c9990e3c41256562b5e2046e3932',
+		'1.4' => '49e7e8bd9effc5a7741509eaaa807794',
+	);
+
+	$theme_hashes[ 'simple' ] = array(
+		'1.1.1' => '7548bfb101e43196c218c6b5533ec5b9',
+		'1.1.2' => '42b2f111b2a68adda611dde3e4297c06',
+		'1.1.3' => '74273da11d028dda51c41901c815b49c',
+		'1.1' => 'c1da063816962e4aab78aa5f86346508',
+		'1.2.1' => '95e66b63f3ab3ffdd03f5f12a001e7fa',
+		'1.2' => 'e777c8a8ecb159b49df4bd8e2da1e69b',
+		'1.3' => '5f900337728c9bae5d3f10dc536a0434',
+		'1.4.1' => '8bc4456db9bb3568331ef295f4512c03',
+		'1.4.2' => '9e103ca85d267f8dcc2147f0e54cc21a',
+		'1.4.3' => '0533ace83db8028d95c5510049449323',
+		'1.4' => '51c4c86e539ed6b6fe7534fec306aa15',
+	);
+
+	return $theme_hashes;
+}
