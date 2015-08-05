a017201aaf2b5c3c8fc1233da3b4e650d4ce0e9e
bravenewcode <bravenewcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-05 17:37:30 +0000

    Cleanup for 3.8.3 release
    git-svn-id: https://plugins.svn.wordpress.org/wptouch/trunk@1213628 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- themes/foundation/modules/base/preview-bar.php
+++
@@ -1,5 +0,0 @@
-<style type="text/css">#switch { display: none; }</style>
-<div id="preview-bar">
-	<p><?php echo wptouchize_it( 'WPtouch Pro' ); ?> <?php _e( 'Theme Preview', 'wptouch-pro' ); ?></p>
-	<div class="refresher"><?php _e( 'Reload', 'wptouch-pro' ); ?></div>
-</div>
